6bc6f15bfbe7ef47091beea942a8bc8f1ecdc3dd
Mahmoud Mansour <mahmodlte@gmail.com>
2020-05-13 08:40:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
+prepend.unshift("Milo");
+
